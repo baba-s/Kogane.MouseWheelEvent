63206494ba3c739bd7239e640c7e821cd5bb224d
baba-s <shotababa1206@gmail.com>
2022-12-03 10:38:05 +0900

    add

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# Kogane.MouseWheelEvent
+# Kogane Mouse Wheel Event
+
+マウスホイールのイベントを管理するクラス
+
+## 使用例
+
+```csharp
+using Kogane;
+using UnityEngine;
+
+public sealed class Example : MonoBehaviour
+{
+    [SerializeField] private MouseWheelEvent m_mouseWheelEvent;
+
+    private void Awake()
+    {
+        m_mouseWheelEvent.OnUp   = () => Debug.Log( "Up" );
+        m_mouseWheelEvent.OnDown = () => Debug.Log( "Down" );
+    }
+}
+```
---
+++ Runtime/MouseWheelEvent.cs
@@ -0,0 +1,48 @@
+﻿#if UNITY_EDITOR
+
+using System;
+using UnityEngine;
+
+namespace Kogane
+{
+    /// <summary>
+    /// マウスホイールのイベントを管理するクラス
+    /// </summary>
+    public sealed class MouseWheelEvent : MonoBehaviour
+    {
+        //================================================================================
+        // プロパティ
+        //================================================================================
+        public Action OnUp   { get; set; }
+        public Action OnDown { get; set; }
+
+        //================================================================================
+        // 関数
+        //================================================================================
+        /// <summary>
+        /// 破棄されるときに呼び出されます
+        /// </summary>
+        private void OnDestroy()
+        {
+            OnUp   = null;
+            OnDown = null;
+        }
+
+        /// <summary>
+        /// 毎フレーム呼び出されます
+        /// </summary>
+        private void Update()
+        {
+            var callback = Input.mouseScrollDelta.y switch
+            {
+                < 0 => OnDown,
+                > 0 => OnUp,
+                _   => default,
+            };
+
+            callback?.Invoke();
+        }
+    }
+}
+
+#endif
